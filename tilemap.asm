aaae2bddb222ae9dd7131b53d5bbcc49e8596cb6
Vivien B <vivienbihl@gmail.com>
2020-05-24 17:37:38 +0200

    Indent

--- tilemap.asm
+++ tilemap.asm
@@ -10,57 +10,35 @@
 .org 018000
 .base 8000
 
-tileset:
-    .incbin assets/tileset.bin
-tileset_palette:
-    .incbin assets/tileset-pal.bin
-small_map:
-    .incbin assets/small.bin
-medium_map:
-    .incbin assets/medium.bin
-big_map:
-    .incbin assets/big.bin
+tileset:                  .incbin assets/tileset.bin
+tileset_palette:          .incbin assets/tileset-pal.bin
+small_map:                .incbin assets/small.bin
+medium_map:               .incbin assets/medium.bin
+big_map:                  .incbin assets/big.bin
 
 .org 7e0000
-joy1_raw:
-    .rb 2
-joy1_press:
-    .rb 2
-joy1_held:
-    .rb 2
-screen_x_velocity:
-    .rb 2
-screen_y_velocity:
-    .rb 2
-screen_tm_x:            ; screen position relative to tilemap
-    .rb 1
-screen_tm_y:
-    .rb 1
-prev_screen_tm_x:
-    .rb 1
-prev_screen_tm_y:
-    .rb 1
-screen_m_x:             ; screen position relative to map
-    .rb 2
-screen_m_y:
-    .rb 2
-prev_screen_m_x:
-    .rb 2
-prev_screen_m_y:
-    .rb 2
 
-current_map:            ; pointer to current map (map should always be in same bank)
-    .rb 3
-current_map_width:      ; width of current map (in tiles)
-    .rb 2
-current_map_height:     ; height of current map (in tiles)
-    .rb 2
-current_map_width_pixel:
-    .rb 2
-current_map_height_pixel:
-    .rb 2
-tilemap_buffer:
-    .rb 800
+joy1_raw:                 .rb 2
+joy1_press:               .rb 2
+joy1_held:                .rb 2
+screen_x_velocity:        .rb 2
+screen_y_velocity:        .rb 2
+screen_tm_x:              .rb 1           ; screen position relative to tilemap
+screen_tm_y:              .rb 1
+prev_screen_tm_x:         .rb 1
+prev_screen_tm_y:         .rb 1
+screen_m_x:               .rb 2           ; screen position relative to map
+screen_m_y:               .rb 2
+prev_screen_m_x:          .rb 2
+prev_screen_m_y:          .rb 2
+
+current_map:              .rb 3           ; pointer to current map (map should always be in same bank)
+current_map_width:        .rb 2           ; width of current map (in tiles)
+current_map_height:       .rb 2           ; height of current map (in tiles)
+
+current_map_width_pixel:  .rb 2
+current_map_height_pixel: .rb 2
+tilemap_buffer:           .rb 800
 
 .org 008000
 .base 0000
