f1c95fcc40b4590007de140b4dfff4d9a7722fc0
Vivien B <vivienbihl@gmail.com>
2020-12-28 19:49:39 +0100

    Add simpler spritesheet for now

--- assets.asm
+++ assets.asm
@@ -6,5 +6,5 @@ tileset_palette:          .incbin assets/pkmn-pal.bin
 small_map:                .incbin assets/small.csv.bin
 medium_map:               .incbin assets/medium.csv.bin
 big_map:                  .incbin assets/pkmn.csv.bin
-spritesheet:              .incbin assets/spritesheet.bin
-spritesheet_pal:          .incbin assets/spritesheet-pal.bin
+spritesheet:              .incbin assets/red.bin
+spritesheet_pal:          .incbin assets/red-pal.bin
